d60aea3bfefd82b14579c9e17e1a86500f938fa7
bEEnsRus <tt4356212@gmail.com>
2025-09-09 07:57:57 +0900

    testing

---
+++ TEMP/Assets/_GAME/_Player/_Player_Scripts/PMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class PMovement : MonoBehaviour
+{
+    #region Data
+    [SerializeField] private Vector2 _playerPos;
+    private Vector2 _playerDir;
+    #endregion
+    private void CheckKeyPressed()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
