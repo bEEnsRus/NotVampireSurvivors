862a5cd3d38413f6304bf10fb7623b402c68cbe9
bEEnsRus <tt4356212@gmail.com>
2025-11-07 21:54:34 +0900

    adding exp
    need sprite

--- TEMP/Assets/_GAME/Settings/GameStats.cs
+++ TEMP/Assets/_GAME/Settings/GameStats.cs
@@ -5,8 +5,10 @@ public class GameStats : MonoBehaviour
     public static int playerHp;
     public static int enemyCount;
     public static int playerAttack;
+    public static int playerExp;
     void OnEnable()
     {
+        playerExp = 0;
         playerHp = 100;
         enemyCount = 0;
         playerAttack = 10;
---
+++ TEMP/Assets/_GAME/_Enemy/_Enemy_Scripts/ExpCollider.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class ExpCollider : MonoBehaviour
+{
+    private GameObject player;
+    private bool shouldMove;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameStats.playerExp++;
+            Destroy(this.gameObject);
+        }
+        if (!collision.gameObject.CompareTag("PlayerCollector"))
+            return;
+        shouldMove = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("PlayerCollector"))
+            return;
+        shouldMove = false;
+    }
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    private void Update()
+    {
+        if (!shouldMove)
+            return;
+        gameObject.transform.position = Vector2.Lerp(gameObject.transform.position, player.transform.position, Time.deltaTime * 2f);
+
+    }
+    //deprecated code under \/
+    /*#region GoToPlayer protocol
+    private IEnumerator coroutine;
+    IEnumerator GoToPlayer(GameObject player)
+    {
+        while (true)
+        {
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, player.transform.position, 5*Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    #endregion
+    #region OnTriggerEnter
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameStats.playerExp++;
+            Destroy(this.gameObject);
+        }
+        if (!collision.gameObject.CompareTag("PlayerCollector"))
+            return;
+        Debug.LogWarning("triggered");
+        coroutine = GoToPlayer(collision.gameObject);
+        StartCoroutine(coroutine);
+    }
+    #endregion
+    #region OnTriggerExit
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("PlayerCollector"))
+            return;
+        StopCoroutine(coroutine);
+        Debug.LogWarning("untriggered");
+    }
+    #endregion*/
+}
